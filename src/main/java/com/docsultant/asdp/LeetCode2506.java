646c7e4e21a6e92b363d6b62d42c5675bdd77450
Vadim Melnik <vmelnik@docsultant.com>
2023-05-09 12:41:22 +0300

    ADSP permanent commits

--- src/main/java/com/docsultant/asdp/LeetCode2506.java
+++ src/main/java/com/docsultant/asdp/LeetCode2506.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @Slf4j
 public class LeetCode2506
 {
@@ -57,10 +58,44 @@ public class LeetCode2506
 		}
 	}
 
+	public static class TaskStream1 implements Task
+	{
+		@Override
+		public int countPairs(List<String> lst) throws Exception
+		{
+			log.debug("countPairs(lst=...)");
+			int res = 0;
+			if( lst==null )
+				return res;
+
+			if( lst.size()<2 )
+				return res;
+
+			Map<Integer, Integer> map = new HashMap<>();
+			lst.stream()
+				.filter(s-> s!=null && s.length()>0 )
+				.forEach(s -> {
+					int hash = s.chars()
+								.reduce(0, (v, ch) -> v |= 1 << ((int) ch - (int) 'a'));
+					map.put(hash, map.computeIfAbsent(hash, k -> 0) + 1);
+				});
+
+			res = map.values().stream()
+					.reduce(0, (n, c) -> n + c * (c-1) / 2 );
+
+			return res;
+		}
+	}
+
 	public static void main(String[] args) throws Exception
 	{
 		Task t = new TaskPlain();
-		t.countPairs(Arrays.asList("aba", "aabb", "abcd", "bac", "aabc"));
+		int n = t.countPairs(Arrays.asList("aba", "aabb", "abcd", "bac", "aabc"));
+		log.debug("plain, n="+n);
+
+		t = new TaskStream1();
+		n = t.countPairs(Arrays.asList("aba", "aabb", "abcd", "bac", "aabc"));
+		log.debug("stream1, n="+n);
 	}
 
 }
--- src/test/java/com/docsultant/asdp/LeetCode2506Test.java
+++ src/test/java/com/docsultant/asdp/LeetCode2506Test.java
@@ -21,4 +21,16 @@ public class LeetCode2506Test
 		assertEquals(res, 0);
 	}
 
+	@Test
+	public void testStream() throws Exception {
+		int res = new LeetCode2506.TaskPlain().countPairs(Arrays.asList("aba", "aabb", "abcd", "bac", "aabc"));
+		assertEquals(res, 2);
+
+		res = new LeetCode2506.TaskPlain().countPairs(Arrays.asList("aabb","ab","ba"));
+		assertEquals(res, 3);
+
+		res = new LeetCode2506.TaskPlain().countPairs(Arrays.asList("nba","cba","dba"));
+		assertEquals(res, 0);
+	}
+
 }
